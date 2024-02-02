9680bc7329a971c9bfa94cda81836a470befc14b
balex <balexvicx@gmail.com>
2024-02-02 12:57:31 +0100

    01 add thread but crash

--- app/src/main/java/com/balex/coroutine/MainActivity.kt
+++ app/src/main/java/com/balex/coroutine/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.balex.coroutine
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
@@ -8,6 +9,7 @@ import com.balex.coroutine.databinding.ActivityMainBinding
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
@@ -24,31 +26,40 @@ class MainActivity : AppCompatActivity() {
     private fun loadData() {
         binding.progress.isVisible = true
         binding.buttonLoad.isEnabled = false
-        val city = loadCity()
-        binding.tvLocation.text = city
-        val temp = loadTemperature(city)
-        binding.tvTemperature.text = temp.toString()
-        binding.progress.isVisible = false
-        binding.buttonLoad.isEnabled = true
+        Log.d(TAG, "1")
+        loadCity { it ->
+            binding.tvLocation.text = it
+            Log.d(TAG, "2")
+            loadTemperature(it) {
+                binding.tvTemperature.text = it.toString()
+                binding.progress.isVisible = false
+                binding.buttonLoad.isEnabled = true
+            }
+            Log.d(TAG, "3")
+        }
+        Log.d(TAG, "4")
 
     }
 
 
-    private fun loadCity(): String {
-        Thread.sleep(3000)
-        return "Bern"
+    private fun loadCity(callback: (String) -> Unit) {
+        thread {
+            Thread.sleep(3000)
+            callback.invoke("Bern")
+        }
+
     }
 
-    private fun loadTemperature(city: String): Int {
-
-        Toast.makeText(
-            this,
-            getString(R.string.loading_temperature_toast, city),
-            Toast.LENGTH_SHORT
-        ).show()
-        Thread.sleep(3000)
-        return 17
-
+    private fun loadTemperature(city: String, callback: (Int) -> Unit) {
+        thread{
+            Toast.makeText(
+                this,
+                getString(R.string.loading_temperature_toast, city),
+                Toast.LENGTH_SHORT
+            ).show()
+            Thread.sleep(3000)
+            callback.invoke(17)
+        }
     }
 
 }
